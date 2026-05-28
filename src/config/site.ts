f94ef7ced08b21040c01f68bf58c41718894ed16
BaeSeongwon <seongwon9179@gmail.com>
2026-05-29 00:19:52 +0900

    구글 연동 실패 이슈 수정

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,8 +1,8 @@
 export const siteConfig = {
   appName: '콜메모',
-  version: '1.1.1',
+  version: '1.1.2',
   publishedAt: '2026.05.25',
-  fileSizeMb: '19.3',
+  fileSizeMb: '53.3',
 } as const
 
 export function getApkFileName(version: string = siteConfig.version): string {
